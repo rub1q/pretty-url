30eb5ebc379dbf80d1e84f29d86900d6e29608a5
rub1q <rub1q@example.com>
2024-11-28 16:53:43 +0300

    add application::run() call

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,18 @@
+#include "prettyurl/app.hpp"
+
+#include <stdexcept>
+#include <iostream>
+
 int main(int argc, char* argv[]) {
-  return 0;
+  try {
+    prettyurl::application::run();
+  } catch (const std::exception& e) {
+    std::cerr << "an error occured while running app (" << e.what() << ")\n";
+    return EXIT_FAILURE;
+  } catch (...) {
+    std::cerr << "an unknown error occured while running app\n";
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
