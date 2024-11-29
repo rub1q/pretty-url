4b6b4b28d41cc99d4e2f29933c7a8e8decbb50c9
rub1q <rub1q@example.com>
2024-11-29 19:06:26 +0300

    add server start and log release

--- src/app.cpp
+++ src/app.cpp
@@ -3,6 +3,7 @@
 #include "prettyurl/app/logging/log-inl.hpp"
 #include "prettyurl/infra/logging/loggers/console_logger.hpp"
 #include "prettyurl/infra/logging/loggers/file_logger.hpp"
+#include "prettyurl/infra/net/http/server.hpp"
 
 namespace prettyurl {
 
@@ -15,6 +16,19 @@ void application::run() {
 
   clogger->info("start init app");
   flogger->info("start init app");
+
+  infra::net::http::server([](auto&&) {
+    infra::net::http::response resp;
+    
+    resp.status_code(core::net::http::status::ok);
+    resp.body("request received\n");
+    resp.prepare_payload();
+
+    return resp;
+  }).concurrency(4)
+    .listen_and_serve(8080);
+  
+  app::logging::release();
 }
 
 } // namespace prettyurl
