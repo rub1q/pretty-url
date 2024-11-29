0754450563200439d00adb2a040bf12ccd71bce3
rub1q <rub1q@example.com>
2024-11-29 19:03:46 +0300

    add http server interface

---
+++ include/prettyurl/core/net/http/base_server.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstdint>
+
+namespace prettyurl::core::net::http {
+
+class base_server {
+public:
+  virtual ~base_server() = default;
+
+  virtual void listen_and_serve(const std::uint16_t port) = 0;
+  virtual void stop() = 0;
+};
+
+} // namespace prettyurl::core::net::http
