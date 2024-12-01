c742adb56f3c92bf26c4c8b318101449bedda86d
rub1q <rub1q@example.com>
2024-12-01 21:20:23 +0300

    add read_request() and write_response() methods

--- include/prettyurl/core/net/http/base_session.hpp
+++ include/prettyurl/core/net/http/base_session.hpp
@@ -2,12 +2,16 @@
 
 namespace prettyurl::core::net::http {
 
+template <typename RequestT, typename ResponseT>
 class base_session {
 public:
   virtual ~base_session() = default;
 
   virtual void start() = 0;
   virtual void close() = 0;
+
+  virtual void read_request(RequestT&& req) = 0;
+  virtual void write_response(ResponseT&& resp) = 0;
 };
 
 } // namespace prettyurl::core::net::http 
