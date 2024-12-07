dec4e49a40d609abc4cafd5e98239f6de2007671
rub1q <rub1q@example.com>
2024-12-08 02:02:11 +0300

    handler constructor now acepts url_shortener_service

--- include/prettyurl/app/handlers/url_shortener_handler.hpp
+++ include/prettyurl/app/handlers/url_shortener_handler.hpp
@@ -3,12 +3,20 @@
 #include "prettyurl/core/net/request_handler.hpp"
 #include "prettyurl/infra/net/http/request.hpp"
 #include "prettyurl/infra/net/http/response.hpp"
+#include "prettyurl/app/services/url_shortener_service.hpp"
 
 namespace prettyurl::app::handlers {
 
-class url_shortener_handler : public core::net::request_handler<infra::net::http::request, infra::net::http::response> {
+class url_shortener_handler final : public core::net::request_handler<infra::net::http::request, infra::net::http::response> {
 public:
-  infra::net::http::response operator()(infra::net::http::request&& req);
+  explicit url_shortener_handler(std::shared_ptr<services::url_shortener_service> service)
+    : service_(std::move(service)) {
+  }
+
+  infra::net::http::response operator()(infra::net::http::request&& req) final;
+
+private:
+  std::shared_ptr<services::url_shortener_service> service_;
 };
 
 } // namespace prettyurl::app::handlers
