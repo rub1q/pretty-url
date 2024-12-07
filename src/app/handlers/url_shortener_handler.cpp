1622affbe36f1ff3e4930a8b44a1b2e6e58f7dca
rub1q <rub1q@example.com>
2024-12-08 02:03:08 +0300

    add json parsing and returning response

--- src/app/handlers/url_shortener_handler.cpp
+++ src/app/handlers/url_shortener_handler.cpp
@@ -1,33 +1,60 @@
 #include "prettyurl/app/handlers/url_shortener_handler.hpp"
+#include "prettyurl/app/logging/log-inl.hpp"
 
 #include <rapidjson/document.h>
+#include <rapidjson/prettywriter.h>
 
 namespace prettyurl::app::handlers {
 
+using namespace std::literals;
+
 infra::net::http::response url_shortener_handler::operator()(infra::net::http::request&& req) {
-  rapidjson::Document root;
-  root.Parse(req.body().data());
+  try {
+    rapidjson::Document root;
+    root.Parse(req.body().data());
 
-  if (root.HasParseError()) {
-    // return bad request
-  }
+    if (root.HasParseError() || !root.HasMember("longUrl") || !root["longUrl"].IsString()) {
+      // TODO: return bad request
+    }
 
-  if (!root.HasMember("longUrl")) {
-    // return bad request
+    const auto long_url = root["longUrl"].GetString();
+    const auto short_url = service_->shorten(long_url);
+
+    rapidjson::Document resp_body;
+    resp_body.SetObject();
+
+    rapidjson::Value short_url_val(short_url.c_str(), resp_body.GetAllocator());
+    resp_body.AddMember("shortUrl", short_url_val, resp_body.GetAllocator());
+
+    rapidjson::StringBuffer sb;
+    rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(sb);
     
+    resp_body.Accept(writer);    
+
+    infra::net::http::response resp { core::net::http::estatus::ok };
+
+    resp.keep_alive(req.keep_alive());
+    resp.version(req.version());
+    resp.body(sb.GetString());
+    resp.header("Content-Type"sv, 
+      core::net::http::helpers::make_content_type_with_charset(
+        core::net::http::econtent_type::application_json,
+        core::net::http::echarset::utf8
+      )
+    );
+
+    resp.header("Cache-Control"sv, "no-cache"sv);
+    resp.prepare_payload();
+
+    return resp;
+
+  } catch (const std::exception& e) {
+    PU_LOG_ERR("an error occured while processing url shorten ({})", e.what());
+    // TODO: return internal server error
+  } catch (...) {
+    PU_LOG_ERR("an unknown error occured while processing url shorten");
+    // TODO: return internal server error
   }
-
-  if (!root["longUrl"].IsString()) {
-    // return bad request
-  }
-
-  auto long_url = root["longUrl"].GetString();
-
-  // TODO: validate long url
-  
-  infra::net::http::response resp {};
-
-  return resp;
 }
 
 } // namespace prettyurl::app::handlers
