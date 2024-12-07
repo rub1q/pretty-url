53957ed4f18b5354f84f2996a270c5deb964ff27
rub1q <rub1q@example.com>
2024-12-08 02:00:16 +0300

    remove nodiscard for add_short_url method

--- include/prettyurl/core/repositories/url_repository.hpp
+++ include/prettyurl/core/repositories/url_repository.hpp
@@ -9,7 +9,8 @@ struct url_repository {
   virtual ~url_repository() = default;
 
   [[nodiscard]] virtual std::optional<std::string> get_long_url(std::string_view short_url) = 0;
-  [[nodiscard]] virtual void add_short_url(std::string_view short_url, std::string_view long_url) = 0;
+  
+  virtual void add_short_url(std::string_view short_url, std::string_view long_url) = 0;
 };
 
 } // namespace prettyurl::core::repositories
