a50aca37a2309bb03c89e7bcaebc1a26da7251c1
rub1q <rub1q@example.com>
2024-12-08 00:10:42 +0300

    add url repository interface

---
+++ include/prettyurl/core/repositories/url_repository.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <optional>
+#include <string>
+
+namespace prettyurl::core::repositories {
+
+struct url_repository {
+  virtual ~url_repository() = default;
+
+  [[nodiscard]] virtual std::optional<std::string> get_long_url(std::string_view short_url) = 0;
+  [[nodiscard]] virtual void add_short_url(std::string_view short_url, std::string_view long_url) = 0;
+};
+
+} // namespace prettyurl::core::repositories
