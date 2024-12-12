b9433b1d3b335b6b74969616f0e8f07b5750ea3a
rub1q <rub1q@example.com>
2024-12-13 00:50:19 +0300

    add more route tests

--- tests/unit/infra/net/http/route_tests.cpp
+++ tests/unit/infra/net/http/route_tests.cpp
@@ -1,9 +1,9 @@
 #include "../../../../../include/prettyurl/infra/net/http/route.hpp"
 #include <catch2/catch_test_macros.hpp>
 
-SCENARIO("route allowed methods test") {
-  using namespace prettyurl;
+using namespace prettyurl;
 
+SCENARIO("route allowed methods test") {
   infra::net::http::route r(core::net::http::emethod::get, [](auto&& req) {
     infra::net::http::response resp { core::net::http::estatus::ok };
     return resp;
@@ -17,13 +17,30 @@ SCENARIO("route allowed methods test") {
   REQUIRE_FALSE(r.is_allowed_method(core::net::http::emethod::patch));
 }
 
-SCENARIO("route null hanler test") {
-  using namespace prettyurl;
-
+SCENARIO("route null handler test") {
   infra::net::http::route r;
   r.methods(core::net::http::emethod::get);
 
   infra::net::http::request req;
 
   REQUIRE_THROWS(r.handle(std::move(req)));
+}
+
+SCENARIO("build new route") {
+  infra::net::http::route r;
+
+  r.methods(core::net::http::emethod::post | core::net::http::emethod::put);
+  r.handler([](auto&& req) { 
+    infra::net::http::response resp { core::net::http::estatus::ok };
+    return resp;
+  });
+
+  REQUIRE(r.is_allowed_method(core::net::http::emethod::head));
+  REQUIRE(r.is_allowed_method(core::net::http::emethod::post));
+  REQUIRE(r.is_allowed_method(core::net::http::emethod::put));
+  
+  infra::net::http::request req;
+  req.method(core::net::http::emethod::post);
+
+  REQUIRE(r.handle(std::move(req)).status_code() == core::net::http::estatus::ok);
 }
