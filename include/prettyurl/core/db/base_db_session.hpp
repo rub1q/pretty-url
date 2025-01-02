dca8fbb5df7e7b72c2a9d2bef54bf47718e1c8b2
rub1q <rub1q@example.com>
2025-01-02 21:35:25 +0300

    add db_session interface

---
+++ include/prettyurl/core/db/base_db_session.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "prettyurl/core/db/query_result_set.hpp"
+
+#include <string>
+#include <optional>
+
+namespace prettyurl::core::db {
+
+class base_db_session {
+public:
+  virtual ~base_db_session() = default;
+
+  [[nodiscard]] virtual bool is_connected() const = 0;
+  [[nodiscard]] virtual std::optional<query_result_set> execute(std::string_view query) = 0;
+
+  virtual void reconnect() = 0;
+
+  virtual void begin_transaction() = 0;
+  virtual void commit_transaction() = 0;
+  virtual void rollback_transaction() = 0;
+
+protected:
+  virtual void connect() = 0;
+  virtual void disconnect() = 0;
+};
+
+} // namespace prettyurl::core::db
