294729e4e20213b738c93e83fa044c8e4b9f3985
rub1q <rub1q@example.com>
2025-01-02 21:39:13 +0300

    add db session manager dependency

--- include/prettyurl/infra/db/repository/pg_url_repository_impl.hpp
+++ include/prettyurl/infra/db/repository/pg_url_repository_impl.hpp
@@ -1,16 +1,27 @@
 #pragma once
 
 #include "prettyurl/core/repositories/url_repository.hpp"
+#include "prettyurl/core/db/db_session_manager.hpp"
+
+#include "prettyurl/infra/db/pg_session.hpp"
 
 namespace prettyurl::infra::db::repository {
 
-struct pg_url_repository_impl final : public core::repositories::url_repository {
+class pg_url_repository_impl final : public core::repositories::url_repository {
+public:
+  explicit pg_url_repository_impl(core::db::db_session_manager<pg_session>& dbsm)
+    : dbsm_(dbsm) {
+  }
+
   [[nodiscard]] std::uint64_t get_last_id() final;
     
   [[nodiscard]] std::optional<std::string> get_long_url(std::string_view short_url) final;
   [[nodiscard]] std::optional<std::string> get_short_url(std::string_view long_url) final;
   
-  void add_short_url(std::string_view short_url, std::string_view long_url) final;  
+  void add_short_url(std::string_view short_url, std::string_view long_url) final;
+
+private:
+  core::db::db_session_manager<pg_session>& dbsm_;
 };
 
 } // namespace prettyurl::infra::db::repository
