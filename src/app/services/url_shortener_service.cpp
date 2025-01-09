f835f3bbbd8dd54872cd60ec6c068f784f2e14b4
rub1q <rub1q@example.com>
2025-01-09 19:41:55 +0300

    add shorten() method impl

--- src/app/services/url_shortener_service.cpp
+++ src/app/services/url_shortener_service.cpp
@@ -1,17 +1,34 @@
 #include "prettyurl/app/services/url_shortener_service.hpp"
+#include "prettyurl/core/utility/url_validator.hpp"
+#include "prettyurl/app/logging/log-inl.hpp"
 
 namespace prettyurl::app::services {
 
 std::string url_shortener_service::shorten(std::string_view src_url) {
   if (!is_valid_url(src_url)) {
-    //
+    throw std::invalid_argument("invalid url format");
   }
 
-  return "hgbQwX8";
+  const auto short_url = repo_->get_short_url(src_url);
+
+  if (short_url.has_value()) {
+    PU_LOG_INF("short url for source url already exists ({})", short_url.value());
+    return short_url.value();
+  }
+
+  const auto id = id_generator_->generate();
+  PU_LOG_INF("new generated id: {}", id);
+
+  const auto encoded_str = encoder_->encode(id);
+  PU_LOG_INF("encoded id: {}", encoded_str);
+
+  repo_->add_short_url(encoded_str, src_url);
+
+  return encoded_str;
 }
 
 bool url_shortener_service::is_valid_url(std::string_view src_url) {
-  return true;
+  return core::utility::url_validator::is_valid(src_url);
 }
 
 } // namespace prettyurl::app::services
