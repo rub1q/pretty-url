b953a3b94e847a2c1846e02e87558e90d36ded58
rub1q <rub1q@example.com>
2025-01-09 22:44:20 +0300

    add sessions_pool_size param

--- include/prettyurl/core/config/db_config.hpp
+++ include/prettyurl/core/config/db_config.hpp
@@ -8,6 +8,7 @@ namespace prettyurl::core::config {
 struct db_config final {
   std::uint16_t port;
   std::uint16_t connect_timeout_sec { 10u };
+  std::uint32_t sessions_pool_size { 5u };
   std::string ip;
   std::string db_name;
   std::string username;
