2d28a8a53bbb1a491478acdab5d4b51cbf9e18ff
rub1q <rub1q@example.com>
2025-01-13 13:03:12 +0300

    update query. add indexes

--- migrations/001_create_url_mapping_table.up.sql
+++ migrations/001_create_url_mapping_table.up.sql
@@ -1,9 +1,11 @@
 create table if not exists url_mapping (
-	id bigserial primary key,
+	id bigint generated always as identity (start with 100000 increment by 1) primary key,
 	short_url varchar(10) unique not null,
-	long_url text not null,
+	long_url text unique not null,
 	created_at timestamp with time zone default current_timestamp,
 	expires_at timestamp with time zone default (current_timestamp + '1 year')
 );
 
 create index idx_expires_at on url_mapping(expires_at);
+create index idx_created_at on url_mapping(created_at);
+create index idx_long_url on url_mapping(long_url);
