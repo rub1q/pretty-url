c9d392ec731e3a3c52967ade9b76b842fbf4da26
rub1q <rub1q@example.com>
2025-01-19 20:43:45 +0300

    add connection to db

--- src/app.cpp
+++ src/app.cpp
@@ -1,5 +1,8 @@
 #include "prettyurl/app.hpp"
 
+#include "prettyurl/core/db/db_session_manager.hpp"
+#include "prettyurl/core/encoding/base62_encoder.hpp"
+
 #include "prettyurl/app/logging/log-inl.hpp"
 #include "prettyurl/infra/logging/loggers/console_logger.hpp"
 #include "prettyurl/infra/logging/loggers/file_logger.hpp"
@@ -9,7 +12,15 @@
 #include "prettyurl/app/handlers/url_shortener_handler.hpp"
 #include "prettyurl/app/services/url_shortener_service.hpp"
 #include "prettyurl/app/services/redirect_url_service.hpp"
+
 #include "prettyurl/infra/db/repository/pg_url_repository_impl.hpp"
+#include "prettyurl/infra/db/pg_session.hpp"
+#include "prettyurl/infra/logging/loggers/console_logger.hpp"
+#include "prettyurl/infra/logging/loggers/file_logger.hpp"
+#include "prettyurl/infra/net/http/server.hpp"
+#include "prettyurl/infra/net/http/router.hpp"
+#include "prettyurl/infra/db/auto_inc_id_generator.hpp"
+#include "prettyurl/infra/db/pg_connect_string_builder.hpp"
 
 namespace prettyurl {
 
@@ -25,7 +36,20 @@ void application::run(const core::config::app_config& cfg) {
   PU_LOG_INF("start init app");
   PU_LOG_INF_TO("console"_logger, "start init app");
 
-  infra::net::http::router router;
+  PU_LOG_INF("connecting to db...");
+  PU_LOG_INF_TO("console"_logger, "connecting to db...");
+
+  infra::db::pg_connect_string_builder cs_builder;
+  
+  cs_builder
+    .host(cfg.db.ip)
+    .port(cfg.db.port)
+    .dbname(cfg.db.db_name)
+    .user(cfg.db.username)
+    .password(cfg.db.password)
+    .connect_timeout(cfg.db.connect_timeout_sec);
+
+  core::db::db_session_manager<infra::db::pg_session> dbs_man(cfg.db.sessions_pool_size, cs_builder.get()); 
 
   auto url_repo = std::make_shared<infra::db::repository::pg_url_repository_impl>(); 
 
