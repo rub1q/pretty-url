7be9df5fc0e9a89d5e01d083a01fcefdf8a5845f
rub1q <rub1q@example.com>
2025-01-19 22:22:43 +0300

    replace routes map with vector. get rid of get() method

--- include/prettyurl/infra/net/http/router.hpp
+++ include/prettyurl/infra/net/http/router.hpp
@@ -19,21 +19,16 @@ public:
     if (path.empty()) {
       throw std::invalid_argument("invalid route path");
     }
-
-    routes_.emplace(path, std::make_shared<route>(methods, std::forward<decltype(handler)>(handler)));
+    
+    routes_.emplace_back(std::make_shared<route>(path, methods, std::forward<decltype(handler)>(handler)));
   }
 
-  [[nodiscard]] std::optional<std::reference_wrapper<const route>> get(std::string_view route_path) const;
+private:
+  [[nodiscard]] response not_found(request&& req) const;
+  [[nodiscard]] response method_not_allowed(request&& req) const;
 
 private:
-  [[nodiscard]] response not_found(const unsigned version, bool keep_alive) const;
-  [[nodiscard]] response method_not_allowed(const unsigned version, bool keep_alive) const;
-
-private:
-  using path = std::string;
-  using routes_tab = std::unordered_map<path, std::shared_ptr<route>>;
-
-  routes_tab routes_;
+  std::vector<std::shared_ptr<route>> routes_;
 };
 
 } // namespace prettyurl::infra::net::http
