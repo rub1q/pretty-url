07f4ae070ab67976fd91b0aff2d2bfcd2a2a990e
rub1q <rub1q@example.com>
2025-01-19 19:17:09 +0300

    add config and cli args parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
 #include "prettyurl/app.hpp"
+#include "prettyurl/app/cli/cli_parser.hpp"
+#include "prettyurl/infra/config/json_config_parser.hpp"
 
 #include <stdexcept>
 #include <iostream>
 
 int main(int argc, char* argv[]) {
   try {
-    prettyurl::application::run();
+    const auto params = prettyurl::app::cli::cli_parser::instance().parse(argc, argv);
+
+    if (!params.has_value()) {
+      throw std::runtime_error("invalid cli params");
+    }
+    
+    prettyurl::core::config::app_config app_cfg;  
+    prettyurl::infra::config::json_config_parser::instance().parse(params.value().config_path, app_cfg);
+    
+    prettyurl::application::run(app_cfg);
   } catch (const std::exception& e) {
     std::cerr << "an error occured while running app (" << e.what() << ")\n";
     return EXIT_FAILURE;
