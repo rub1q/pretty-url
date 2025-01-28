2c5989acf750a18ba611e8e0bf2ff43e5390d737
rub1q <rub1q@example.com>
2025-01-29 00:05:00 +0300

    add cache config

--- include/prettyurl/core/config/app_config.hpp
+++ include/prettyurl/core/config/app_config.hpp
@@ -3,12 +3,14 @@
 #include "prettyurl/core/config/db_config.hpp"
 #include "prettyurl/core/config/logging_config.hpp"
 #include "prettyurl/core/config/server_config.hpp"
+#include "prettyurl/core/config/cache_config.hpp"
 
 namespace prettyurl::core::config {
 
 struct app_config final {
   server_config server;
   db_config db;
+  cache_config cache;
   logging_config logging;
 };
 
